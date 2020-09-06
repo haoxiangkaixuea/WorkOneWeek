660d6bd75916614ab7ad37bd52219b7cd8027e1c
Roger <63828530+haoxiangkaixuea@users.noreply.github.com>
2020-09-06 19:52:29 +0800

    修改UI界面三

--- app/src/main/java/cn/edu/scujcc/workoneweke/AnotherRight.java
+++ app/src/main/java/cn/edu/scujcc/workoneweke/AnotherRight.java
@@ -13,7 +13,6 @@ public class AnotherRight extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.activity_another_right, container, false);
-        return view;
+        return inflater.inflate(R.layout.activity_another_right, container, false);
     }
 }
